4edd2ed0a94c87fe3116196d27c5f881a01782a8
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-06-02 16:30:34 -0300

    feat: update tasks

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -6,5 +6,7 @@ const { verifyToken } = require('../middleware/authMiddleware');
 router.post('/', verifyToken, taskController.createTask);
 router.get('/', taskController.getAllTasks);
 router.get('/:id', taskController.getTaskById);
+router.post('/:id/apply', verifyToken, taskController.applyToTask);
+router.patch('/:taskId/candidates/:candidateId', verifyToken, taskController.updateCandidateStatus);
 
 module.exports = router;
